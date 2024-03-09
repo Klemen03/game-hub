111474e9231629ac8b0ef99235c987339c3da204
Klemen03 <klemenkrklec10@gmail.com>
2024-03-09 14:34:42 +0100

    Building game cards.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '3ff1ee77d7d94733a7a51a7f02a08ce2',
+    key: '177900c1572744c5b26c6320ef86dbae',
   },
 });
